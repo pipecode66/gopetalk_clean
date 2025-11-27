b2ba8c646137ae050a845d707bd0c49d855ecaf5
pipecode66 <pipecode66@users.noreply.github.com>
2025-11-26 19:54:38 -0500

    commit test

--- app/src/main/java/com/example/gopetalk_clean/utils/Constants.kt
+++ app/src/main/java/com/example/gopetalk_clean/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.gopetalk_clean.utils
 
 object Constants {
-    const val BASE_URL = "http://159.203.187.94/"
-    const val WS_URL = "ws://159.203.187.94/ws"
+    const val BASE_URL = "https://api-gopetalk.angularnetweb.online/"
+    const val WS_URL = "wss://api-gopetalk.angularnetweb.online/ws"
 }
